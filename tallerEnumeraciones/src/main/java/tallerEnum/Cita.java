664afca6f771429d8198d2a0e86d9f6703bba51c
Karina <kserranoa16@gmail.com>
2020-09-21 21:40:33 -0600

    Crear enumeracion cita

---
+++ tallerEnumeraciones/src/main/java/tallerEnum/Cita.java
@@ -0,0 +1,18 @@
+
+package tallerEnum;
+
+
+public enum Cita {
+    CLINICA(20), 
+    DOMICILIO(40);
+    
+    private final double citaC;
+    
+    private Cita(final double citaC){
+        this.citaC = citaC;
+    } 
+    
+    public double getCitaC(){
+        return citaC;
+    }
+}
