2b233af656f9e5ee3a0ab927b50dd73e05f546c4
kserranoa <67085518+kserranoa16@users.noreply.github.com>
2020-09-22 12:53:12 -0600

    Agregar opciones de enum

--- tallerEnumeraciones/src/main/java/tallerEnum/Excepciones.java
+++ tallerEnumeraciones/src/main/java/tallerEnum/Excepciones.java
@@ -3,23 +3,63 @@ import java.util.Scanner;
 
 public class Excepciones {
 
-    public void excepcion (Integer opcionCita, Integer opcionTrata, Integer opcionControl, Scanner entrada){
-        //
-       
-       do {
+    public void excepcion (Integer opcionCita, Integer opcionTrata, Integer opcionControl, Integer km, Scanner entrada){
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+        //Ingreso de opcion Cita
+        do {
            try{
+
            System.out.println("Seleccione el tipo de cita \n 1- En Clinica \n 2- A Domicilio");
            opcionCita = entrada.nextInt();
-//           System.out.println("Seleccione el tipo de tratamiento \n 1- Inyeccion \n 2- Pastilla \n 3- Crema");
+           
+        } catch (Exception ex){
+           System.out.println("No puede ingresar texto");
+           entrada.nextLine();
+           } 
+        } while ((0 < opcionCita) && (opcionCita < 3));
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -       
+        //Ingreso de kilometros en caso de existir
+        if (opcionCita == 2){
+            do {
+               try{
+           
+               System.out.println("Ingrese la cantidad de kilometros");
+               km = entrada.nextInt();
+           
+            } catch (Exception ex){
+               System.out.println("No puede ingresar texto");
+               entrada.nextLine();
+               } 
+            } while (km > 0);
+        }
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -       
+    //Ingreso de la opcion tratamiento
+       do {
+           try {
+           System.out.println("Seleccione el tipo de tratamiento \n 1- Inyeccion \n 2- Pastilla \n 3- Crema");
            opcionTrata = entrada.nextInt();
-//           System.out.println("Seleccione el tipo de control \n 1- No Control \n 2- Animal pequeno \n 3- Animal mediano \n 4- Animal grande");
+           
+       } catch (Exception ex){
+           System.out.println("No puede ingresar texto");
+           entrada.nextLine();
+           } 
+       } while ((opcionTrata>0) && (opcionTrata<4));
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -       
+//Ingreso de la opcion Control
+       do {
+           try{
+           System.out.println("Seleccione el tipo de control \n 1- No Control \n 2- Animal pequeno "
+                   + "\n 3- Animal mediano \n 4- Animal grande");
            opcionControl = entrada.nextInt();
            
        } catch(Exception ex){
            System.out.println("No puede ingresar texto");
            entrada.nextLine();
            } 
-       } while (opcionCita<0);
+       } while ((opcionControl > 0) && (opcionControl < 5));
 
     }
 }
