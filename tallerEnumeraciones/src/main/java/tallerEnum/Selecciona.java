5c6745fe5e21649e8e68e576d2f19f5c2c31a33b
kserranoa <67085518+kserranoa16@users.noreply.github.com>
2020-09-22 10:03:36 -0600

    Agrego todo el menu sin return

--- tallerEnumeraciones/src/main/java/tallerEnum/Selecciona.java
+++ tallerEnumeraciones/src/main/java/tallerEnum/Selecciona.java
@@ -1,20 +1,158 @@
-
 package tallerEnum;
-
 
 public class Selecciona {
     
-    public void menu(Integer opcionCita){
+    public void menu(Integer opcionCita, Integer opcionTrata, Integer opcionControl){
         
         switch(opcionCita){
+            //Selecionar cita en la clinica local
             case 1: 
-                System.out.println("Opcion 1");
+                //Selecionar el tipo de tratamiento
+                switch (opcionTrata) {
+                    //Selecionar tratamiento 1 para inyeccion
+                    case 1:
+                        //Selecionar control
+                        switch (opcionControl) {
+                            case 1:
+                                
+                                break;
+                            case 2:
+                                
+                                break;                            
+                            case 3:
+                                
+                                break;                            
+                            case 4:
+                                
+                                break;                            
+                            default:
+                                throw new AssertionError();
+                        }
+                        break;
+                    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+                    //Selecionar tratamiento 2 para pastilla
+                    case 2:
+                        //Selecionar control
+                        switch (opcionControl) {
+                            case 1:
+                                
+                                break;
+                            case 2:
+                                
+                                break;                            
+                            case 3:
+                                
+                                break;                            
+                            case 4:
+                                
+                                break;                            
+                            default:
+                                throw new AssertionError();
+                        }                        
+                        break;                    
+                    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -                        
+                    //Selecionar tratamiento 3 para crema
+                    case 3:
+                        //Selecionar control
+                        switch (opcionControl) {
+                            case 1:
+                                
+                                break;
+                            case 2:
+                                
+                                break;                            
+                            case 3:
+                                
+                                break;                            
+                            case 4:
+                                
+                                break;                            
+                            default:
+                                throw new AssertionError();
+                        }                        
+                        break;
+                        
+                    default:
+                        throw new AssertionError();
+                }
                 break;
+            
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+            //Selecionar cita a domicilio 
             case 2: 
-                System.out.println("Opcion2");
+                //Selecionar el tipo de tratamiento
+                switch (opcionTrata) {
+                    //Selecionar tratamiento 1 para inyeccion
+                    case 1:
+                        //Selecionar control
+                        switch (opcionControl) {
+                            case 1:
+                                
+                                break;
+                            case 2:
+                                
+                                break;                            
+                            case 3:
+                                
+                                break;                            
+                            case 4:
+                                
+                                break;                            
+                            default:
+                                throw new AssertionError();
+                        }
+                        break;
+                    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+                    //Selecionar tratamiento 2 para pastilla
+                    case 2:
+                        //Selecionar control
+                        switch (opcionControl) {
+                            case 1:
+                                
+                                break;
+                            case 2:
+                                
+                                break;                            
+                            case 3:
+                                
+                                break;                            
+                            case 4:
+                                
+                                break;                            
+                            default:
+                                throw new AssertionError();
+                        }                        
+                        break;                    
+                    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -                        
+                    //Selecionar tratamiento 3 para crema
+                    case 3:
+                        //Selecionar control
+                        switch (opcionControl) {
+                            case 1:
+                                
+                                break;
+                            case 2:
+                                
+                                break;                            
+                            case 3:
+                                
+                                break;                            
+                            case 4:
+                                
+                                break;                            
+                            default:
+                                throw new AssertionError();
+                        }                        
+                        break;
+                        
+                    default:
+                        throw new AssertionError();
+                }
                 break;
+                
+            //Seleciono una opcion que no existe
             default: 
-                System.out.println("Default");
+                System.out.println("Opcion no existe, fin del programa");
         } 
     }
 }
