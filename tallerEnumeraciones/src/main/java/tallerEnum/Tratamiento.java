9a4c00f3db315434a9244aed2e5799be6363205f
kserranoa <67085518+kserranoa16@users.noreply.github.com>
2020-09-22 16:34:13 -0600

    Comentar

--- tallerEnumeraciones/src/main/java/tallerEnum/Tratamiento.java
+++ tallerEnumeraciones/src/main/java/tallerEnum/Tratamiento.java
@@ -3,10 +3,12 @@ package tallerEnum;
 
 
 public enum Tratamiento {
+    //Enumeracion para las alternativas de los tratamientos
+    //Estos tratamientos tienen un Tax que se agrega a todos
     INYECCION(25), 
     PASTILLA(15), 
     CREMA(20);
-    
+        
     private final double trataC;
     
     private Tratamiento(final double trataC){
