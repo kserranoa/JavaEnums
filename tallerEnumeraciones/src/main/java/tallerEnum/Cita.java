2182998cc5e30efcaf499fa31c08f52ba7fca7c2
kserranoa <67085518+kserranoa16@users.noreply.github.com>
2020-09-22 16:29:51 -0600

    Comentar

--- tallerEnumeraciones/src/main/java/tallerEnum/Cita.java
+++ tallerEnumeraciones/src/main/java/tallerEnum/Cita.java
@@ -1,17 +1,16 @@
-
 package tallerEnum;
 
-
 public enum Cita {
+    //Enumeracion para las alternativas de citas
     CLINICA(20), 
     DOMICILIO(40);
     
     private final double citaC;
-    
+        
     private Cita(final double citaC){
         this.citaC = citaC;
     } 
-    
+        
     public double getCitaC(){
         return citaC;
     }
