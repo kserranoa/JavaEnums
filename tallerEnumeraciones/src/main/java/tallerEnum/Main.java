d9ba8011162a45bea4460867e5bcbed79d6d2b55
kserranoa <67085518+kserranoa16@users.noreply.github.com>
2020-09-22 12:53:24 -0600

    Agregar opciones de enum

--- tallerEnumeraciones/src/main/java/tallerEnum/Main.java
+++ tallerEnumeraciones/src/main/java/tallerEnum/Main.java
@@ -6,12 +6,12 @@ public class Main {
     public static void main(String[] args) {
 
 
-       Integer opcionCita=0, opcionTrata=0, opcionControl=0;
+       Integer opcionCita=0, opcionTrata=0, opcionControl=0, km=0;
        Scanner entrada = new Scanner(System.in);
        
        //
        Excepciones validarData = new Excepciones();
-       validarData.excepcion(opcionCita, opcionTrata, opcionControl, entrada);
+       validarData.excepcion(opcionCita, opcionTrata, opcionControl, km, entrada);
                
        //
       Selecciona seleccionar = new Selecciona();       
