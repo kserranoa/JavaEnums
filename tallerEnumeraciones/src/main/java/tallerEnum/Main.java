5284923f77508842c6fbca73e687733b2839a1f6
kserranoa <67085518+kserranoa16@users.noreply.github.com>
2020-09-22 16:34:33 -0600

    Comentar

--- tallerEnumeraciones/src/main/java/tallerEnum/Main.java
+++ tallerEnumeraciones/src/main/java/tallerEnum/Main.java
@@ -5,15 +5,15 @@ public class Main {
 
     public static void main(String[] args) {
 
-
+        //Variables a utilizar para calcular costos
        Integer opcionCita=0, opcionTrata=0, opcionControl=0, km=0;
        Scanner entrada = new Scanner(System.in);
        
-       //
+       //Ingresar y validar los datos 
        Excepciones validarData = new Excepciones();
        validarData.excepcion(opcionCita, opcionTrata, opcionControl, km, entrada);
                
-       //
+       //Dependiendo de los datos ingresos se utilizan las enumeraciones
       Selecciona seleccionar = new Selecciona();       
       seleccionar.menu(opcionCita, opcionTrata, opcionControl);
     }
