6e93a3936115588af1104468fc274306eae99f67
Karina <kserranoa16@gmail.com>
2020-09-22 20:40:07 -0600

    Solucion problema cambio de variable Main

--- tallerEnumeraciones/src/main/java/tallerEnum/Main.java
+++ tallerEnumeraciones/src/main/java/tallerEnum/Main.java
@@ -1,21 +1,16 @@
 package tallerEnum;
-import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        //Variables a utilizar para calcular costos
-       Integer opcionCita=0, opcionTrata=0, opcionControl=0, km=0;
-       Scanner entrada = new Scanner(System.in);
-       
        //Ingresar y validar los datos 
        Excepciones validarData = new Excepciones();
-       validarData.excepcion(opcionCita, opcionTrata, opcionControl, km, entrada);
-               
-       //Dependiendo de los datos ingresos se utilizan las enumeraciones
-      Selecciona seleccionar = new Selecciona();       
-      seleccionar.menu(opcionCita, opcionTrata, opcionControl);
+       validarData.excepcion();
+          
+       //Dependiendo de los datos ingresados se utilizan las enumeraciones
+//      Selecciona seleccionar = new Selecciona();       
+//      seleccionar.menu(opcionCita, opcionTrata, opcionControl);
     }
 
 }
