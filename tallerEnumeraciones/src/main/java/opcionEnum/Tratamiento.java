f8f186ec0c64fbb493e5cb5b7e9e149b24db886a
Karina <kserranoa16@gmail.com>
2020-09-24 16:24:26 -0600

    Comentarios agregados

---
+++ tallerEnumeraciones/src/main/java/opcionEnum/Tratamiento.java
@@ -0,0 +1,21 @@
+
+package opcionEnum;
+
+
+public enum Tratamiento {
+    //Enumeracion para las alternativas de los tratamientos
+    //Estos tratamientos tienen un Tax que se agrega a todos
+    INYECCION(25), 
+    PASTILLA(15), 
+    CREMA(20);
+        
+    private final double trataC;
+    
+    private Tratamiento(final double trataC){
+        this.trataC = trataC;
+    }
+    
+    public double getTrataC(){
+        return trataC;
+    }
+}
