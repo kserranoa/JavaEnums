e8dba86359e950b0f6a809f85eeead77c0f8a05e
Karina <kserranoa16@gmail.com>
2020-09-24 16:14:12 -0600

    Comentarios agregados

--- tallerEnumeraciones/src/main/java/tallerEnum/Main.java
+++ tallerEnumeraciones/src/main/java/tallerEnum/Main.java
@@ -3,7 +3,7 @@ package tallerEnum;
 public class Main {
 
     public static void main(String[] args) {
-        
+        //Este for es un bucle infinito, para realizar varias pruebas de funcionamiento
         for (; ;) {
 
         //Ingresar y validar los datos 
@@ -13,11 +13,13 @@ public class Main {
        validarData.addOpcionTrata();
        validarData.addOpcionControl();
        
+       //Datos a utilizar para seleccionar total a mostrar
        Integer opcionCita = validarData.opcionCita;
        Integer opcionTrata = validarData.opcionTrata;
        Integer opcionControl = validarData.opcionControl;
        
        //Dependiendo de los datos ingresados se utilizan las enumeraciones
+      System.out.println("El total es: ");
       Selecciona seleccionar = new Selecciona();       
       seleccionar.menu(opcionCita, opcionTrata, opcionControl);
     }     
