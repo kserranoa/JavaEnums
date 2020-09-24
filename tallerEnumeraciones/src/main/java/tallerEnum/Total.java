f0b11650d6a2e898edb39f49dada3a7a35d7e6af
Karina <kserranoa16@gmail.com>
2020-09-23 21:02:34 -0600

    Arreglo de la variable costoDistancia

--- tallerEnumeraciones/src/main/java/tallerEnum/Total.java
+++ tallerEnumeraciones/src/main/java/tallerEnum/Total.java
@@ -9,16 +9,16 @@ public class Total {
     // La variable taxTratamiento es el impuesto del 13% de la clase Tratamiento
     double taxTratamiento = 1.13;
     
+    //En caso de un servicio a domicilio calcular costo por distancia 
+    Excepciones calculoDistancia = new Excepciones();
+    double costoDistancia = calculoDistancia.costoDistancia;
+    
     public Total(final Cita cita, final Tratamiento tratamiento, final Control control){
         this.cita = cita;
         this.tratamiento = tratamiento;
         this.control = control;
     }
-
-    //En caso de un servicio a domicilio calcular costo por distancia 
-    Excepciones calculoDistancia = new Excepciones();
-    Integer costoDistancia = calculoDistancia.costoDistancia;
-        
+    
     public double costo(){
         return (cita.getCitaC() + costoDistancia+ (tratamiento.getTrataC()*taxTratamiento) + control.getControlC());
     }
