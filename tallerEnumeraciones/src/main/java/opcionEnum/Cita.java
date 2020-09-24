d6a8af3d4699e6749e1268f91fb75366669d1810
Karina <kserranoa16@gmail.com>
2020-09-24 16:24:46 -0600

    Comentarios agregados

---
+++ tallerEnumeraciones/src/main/java/opcionEnum/Cita.java
@@ -0,0 +1,17 @@
+package opcionEnum;
+
+public enum Cita {
+    //Enumeracion para las alternativas de citas
+    CLINICA(20), 
+    DOMICILIO(40);
+    
+    private final double citaC;
+        
+    private Cita(final double citaC){
+        this.citaC = citaC;
+    } 
+        
+    public double getCitaC(){
+        return citaC;
+    }
+}
